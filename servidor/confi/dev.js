8513861bc56ebfafa844e8a4a1d531c7ec9a7b0b
chris-Sanchez098 <53357588+chris-Sanchez098@users.noreply.github.com>
2021-09-30 23:18:32 -0500

    Add files via upload

--- servidor/confi/dev.js
+++ servidor/confi/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    postgresurl: 'postgres://njfncwhrqjlctp:c86fbba1307b08884ec017ede074c6874f7d7184383c730a85f328d7dfba8075@ec2-44-195-247-84.compute-1.amazonaws.com:5432/dd07dcrtokuip3'
+    postgresurl: 'postgres://mdbfaxjg:db5XxiURErsxsT0b9mfZYxaCnIdt1EK6@fanny.db.elephantsql.com/mdbfaxjg'
 }
--- servidor/server.js
+++ servidor/server.js
@@ -18,17 +18,78 @@ const db = new Client({
 
 db.connect();
 
-app.post("/api/insert", async (req, res) => {
-  const {nombre, apellido, correo} = req.body
+app.post("/api/insert/user", (req, res) => {
+  const {nombre, apellido, correo, sexo} = req.body
 
-  const pgInsert = `INSERT INTO usuario (nombre, apellido, correo) 
-                    VALUES('${nombre}','${apellido}','${correo}')`; 
+  const pgInsert = `INSERT INTO usuario (nombre, apellido, correo,sexo) 
+                    VALUES('${nombre}','${apellido}','${correo}','${sexo}')`; 
 
 db.query(
   pgInsert, (err, result) =>{
     console.log(err);
   })
 });
+
+app.post("/api/insert/music", (req, res) => {
+  const {genero_musica, horas_musica, razon_musica, bailas} = req.body
+
+  const pgInsert = `INSERT INTO musica (genero_musica, horas_musica, razon_musica, bailas) 
+                    VALUES('${genero_musica}','${horas_musica}','${razon_musica}','${bailas}')`; 
+
+db.query(
+  pgInsert, (err, result) =>{
+    console.log(err);
+  })
+});
+
+app.post("/api/insert/concert", (req, res) => {
+  const {genero_concierto, horario_concierto, num_artista,lugar_concierto, dia_concierto, duracion_concierto} = req.body
+
+  const pgInsert = `INSERT INTO concierto (genero_concierto, horario_concierto, num_artista,lugar_concierto, dia_concierto, duracion_concierto) 
+                    VALUES('${genero_concierto}','${horario_concierto}','${num_artista}','${lugar_concierto}','${dia_concierto}','${duracion_concierto}')`; 
+
+db.query(
+  pgInsert, (err, result) =>{
+    console.log(err);
+  })
+});
+
+app.post("/api/insert/cine", (req, res) => {
+  const {genero_cine, num_peliculas, horario_pelicula,lugar_cine,dia_cine,duracion_cine,mecato} = req.body
+
+  const pgInsert = `INSERT INTO cine (genero_cine, num_peliculas, horario_pelicula,lugar_cine,dia_cine,duracion_cine,mecato) 
+    VALUES('${genero_cine}','${num_peliculas}','${horario_pelicula}','${lugar_cine}','${dia_cine}','${duracion_cine}','${mecato}')`; 
+
+db.query(
+  pgInsert, (err, result) =>{
+    console.log(err);
+  })
+});
+
+app.post("/api/insert/car", (req, res) => {
+  const {marca, tiene_carro, puertas, lugar, dia, masrca, cambiar} = req.body
+  
+  const pgInsert = `INSERT INTO carro (marca_favorita, tiene_carro, num_puertas,lugar_carro,dia_carro,marca_poseida,cambiar_carro) 
+    VALUES('${marca}','${tiene_carro}','${puertas}','${lugar}','${dia}','${masrca}','${cambiar}')`; 
+
+db.query(
+  pgInsert, (err, result) =>{
+    console.log(result);
+  })
+});
+
+app.post("/api/insert/sport", (req, res) => {
+  const {deporte, hora, exp, rutina, lugar} = req.body
+
+  const pgInsert = `INSERT INTO deporte (deporte_favorito, horas_practica, malas_experiencias,rutinas,lugar_deporte) 
+                    VALUES('${deporte}','${hora}','${exp}','${rutina}','${lugar}')`; 
+
+db.query(
+  pgInsert, (err, result) =>{
+    console.log(err);
+  })
+});
+
 
 // create a GET route
 /*
